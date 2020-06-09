54886918afe98a5a548cb2b4659baf79eee93414
Emmanuel Lokossou <sewonou.lokossou1988@gmail.com>
2020-06-09 12:55:52 +0000

    Légère mise à jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\DayEvent;
 use App\Entity\Participant;
 use App\Form\ParticipantType;
 use App\Form\RegistrationType;
+use App\Repository\CategoryRepository;
+use App\Repository\ContentRepository;
+use App\Repository\DayRepository;
+use App\Repository\FaqRepository;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,13 +22,21 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param CategoryRepository $categoryRepository
+     * @param ContentRepository $contentRepository
+     * @param DayRepository $dayRepository
+     * @param FaqRepository $faqRepository
+     * @return Response
      */
-    public function index()
+    public function index(CategoryRepository $categoryRepository, ContentRepository $contentRepository, DayRepository $dayRepository,FaqRepository $faqRepository)
     {
+        $about = $categoryRepository->findOneBy(['title' => 'ABOUT']);
 
 
         return $this->render('home/index.html.twig', [
-
+            'abouts' => $contentRepository->findBy(['category' => $about]),
+            'faqs' => $faqRepository->findAll(),
+            'days' => $dayRepository->findAll(),
         ]);
     }
 
